b881d831cefec71e858dc23ded6a74366647a22c
mansanchez <manuel_724@liv.com.mx>
2022-11-15 19:42:51 -0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import org.opencv.core.Core;
+
+public class Main {
+    public static void main(String[] args) {
+        // Load the native OpenCV library
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        // Schedule a job for the event dispatch thread:
+        // creating and showing this application's GUI.
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new FindContours(args);
+            }
+        });
+
+    }
+}
